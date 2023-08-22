4555560725a6b6b4b16b445999ef83f8e8d3f443
Alex Merchant <unconfigured@noexist.com>
2023-08-21 22:51:08 -0500

    Bonus Mission: Added validation to check for non-numeric and negative values using do-while loop

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -6,10 +6,33 @@ public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter a radius: ");
-        double radius = input.nextDouble();
+        double radius;
+        int negativeValueAttempts = 0;
+
+        do {
+            // Input prompt runs on first attempt only.
+            if (negativeValueAttempts == 0) {
+                System.out.println("Enter a radius: ");
+            }
+
+            // Validates input value to make sure it is a valid double value
+            while (!input.hasNextDouble()) {
+                System.out.println("That value is not a number. Try again: ");
+                input.next(); // Consume the non-double input
+            }
+
+            // radius is assigned input value if above validation passes
+            radius = input.nextDouble();
+
+            // If double value is negative, prompt again for positive number and increase token count to bypass initial prompt message and use specific prompt
+            if (radius < 0) {
+                System.out.println("Enter a non-negative number. Try again: ");
+                negativeValueAttempts++;
+            }
+        } while (radius < 0);
 
         System.out.println("The area of a circle of radius " + radius + " is: " + Circle.getArea(radius));
+
         input.close();
     }
 }
