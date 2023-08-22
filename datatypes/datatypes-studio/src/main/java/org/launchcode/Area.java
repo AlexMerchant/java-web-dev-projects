afb63adec32d71bec935d2c31d80e6a6b304526e
Alex Merchant <unconfigured@noexist.com>
2023-08-21 21:18:34 -0500

    Finished studio again but fixed repo branches

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -1,2 +1,15 @@
-package org.launchcode;public class Area {
+package org.launchcode;
+
+import studios.areaofacircle.Circle;
+import java.util.Scanner;
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a radius: ");
+        double radius = input.nextDouble();
+
+        System.out.println("The area of a circle of radius " + radius + " is: " + Circle.getArea(radius));
+        input.close();
+    }
 }
