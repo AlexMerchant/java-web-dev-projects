93355fd48a9e4251a0f002bb83602ba4639e2772
Alex Merchant <unconfigured@noexist.com>
2023-09-11 23:21:18 -0500

    Redesigned hasBalancedBrackets() algorithm. Created unit tests for studio.

--- unit-testing/studio/balanced-brackets/src/main/java/org/launchcode/BalancedBrackets.java
+++ unit-testing/studio/balanced-brackets/src/main/java/org/launchcode/BalancedBrackets.java
@@ -1,4 +1,7 @@
 package org.launchcode;
+
+import java.util.ArrayList;
+
 public class BalancedBrackets {
     /*
      * The function BalancedBrackets should return true if and only if
@@ -19,15 +22,71 @@ public class BalancedBrackets {
      * @param str - to be validated
      * @return true if balanced, false otherwise
      */
+
+    /*
+     *** START LAUNCHCODE PROVIDED STARTER CODE
+        public static boolean hasBalancedBrackets(String str) {
+            int brackets = 0;
+            for (char ch : str.toCharArray()) {
+                if (ch == '[') {
+                    brackets++;
+                } else if (ch == ']') {
+                    brackets--;
+                }
+            }
+            return brackets == 0;
+        }
+     ***  end LAUNCHCODE PROVIDED STARTER CODE
+    */
+
     public static boolean hasBalancedBrackets(String str) {
-        int brackets = 0;
+        ArrayList<Character> openingBrackets = new ArrayList<>();
+        ArrayList<Character> closingBrackets = new ArrayList<>();
+
         for (char ch : str.toCharArray()) {
-            if (ch == '[') {
-                brackets++;
-            } else if (ch == ']') {
-                brackets--;
+            if (ch == '[' || ch == '{' || ch == '(') {
+                openingBrackets.add(ch);
+            } else if (ch == ']' || ch == '}' || ch == ')') {
+                closingBrackets.add(ch);
             }
         }
-        return brackets == 0;
+
+        int openingBracketsIndex = openingBrackets.size() - 1;
+        int closingBracketsIndex = 0;
+
+        while(openingBracketsIndex >= 0 && closingBracketsIndex < closingBrackets.size()) {
+            if(openingBrackets.size() != closingBrackets.size()) {
+                return false;
+
+            } else if (openingBrackets.get(openingBracketsIndex) == '[' && closingBrackets.get(closingBracketsIndex) == ']') {
+                System.out.println("Braces match.");
+
+                openingBracketsIndex--;
+                closingBracketsIndex++;
+
+            } else if (openingBrackets.get(openingBracketsIndex) == '{' && closingBrackets.get(closingBracketsIndex) == '}') {
+                System.out.println("Braces match.");
+
+                openingBracketsIndex--;
+                closingBracketsIndex++;
+
+            } else if (openingBrackets.get(openingBracketsIndex) == '(' && closingBrackets.get(closingBracketsIndex) == ')') {
+                System.out.println("Braces match.");
+
+                openingBracketsIndex--;
+                closingBracketsIndex++;
+
+            } else {
+                System.out.println("Braces mismatch");
+                return false;
+            }
+        }
+
+        return true;
+
     }
+
+//    public static void main(String[] args) {
+//        System.out.println(BalancedBrackets.hasBalancedBrackets("[[]"));
+//    }
 }
--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -10,4 +10,46 @@ class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    // TODO: More opening brackets (positive)
+    @Test
+    public void moreOpeningThanClosingBrackets() {
+        boolean expected = false;
+        boolean actual = BalancedBrackets.hasBalancedBrackets("[[]");
+        assertEquals(expected, actual);
+    }
+
+    // TODO: More close brackets (negative)
+    @Test
+    public void moreClosingThanOpeningBrackets() {
+        boolean expected = false;
+        boolean actual = BalancedBrackets.hasBalancedBrackets("[]]");
+        assertEquals(expected, actual);
+    }
+
+    // TODO: A string containing more than just brackets that is also balanced should return true.
+    @Test
+    public void stringWithBalancedBracketsReturnsTrue() {
+        boolean shouldBeTrue = BalancedBrackets.hasBalancedBrackets("This String has [a balanced [set of] brackets].");
+        assertTrue(shouldBeTrue);
+    }
+
+    // TODO: A string containing more than just brackets that are not balanced should return false.
+    @Test
+    public void stringWithUnbalancedBracketsReturnsFalse() {
+        boolean shouldBeFalse = BalancedBrackets.hasBalancedBrackets("This String has ]an unbalanced [set of] brackets].");
+        assertFalse(shouldBeFalse);
+    }
+
+    // TODO: A string contains a balanced set of brackets but which are improperly nested, should return false
+    @Test
+    public void stringWithImproperlyNestedBracketsReturnsFalse() {
+        String str = "][[]][][";
+        boolean shouldBeFalse = BalancedBrackets.hasBalancedBrackets(str);
+        assertFalse(shouldBeFalse);
+    }
 }
