b89dd29fed046d1d9a7106fc3ce7340fca032353
Alex M <alex@merchant.dev>
2023-09-12 20:18:38 -0500

    - Added in-line comments to better describe algorithm approach

--- unit-testing/studio/balanced-brackets/src/main/java/org/launchcode/BalancedBrackets.java
+++ unit-testing/studio/balanced-brackets/src/main/java/org/launchcode/BalancedBrackets.java
@@ -36,82 +36,68 @@ public class BalancedBrackets {
             }
             return brackets == 0;
         }
-     ***  end LAUNCHCODE PROVIDED STARTER CODE
+     ***  END LAUNCHCODE PROVIDED STARTER CODE
     */
 
     public static boolean hasBalancedBrackets(String str) {
         ArrayList<Character> openingBrackets = new ArrayList<>();
-        ArrayList<Character> closingBrackets = new ArrayList<>();
 
         int brackets = 0;
 
         for (char ch : str.toCharArray()) {
             if (ch == '[' || ch == '{' || ch == '(') {
+                // If an opener is encountered, add it to the end of the openingBrackets ArrayList, increase brackets by 1
                 openingBrackets.add(ch);
                 brackets++;
 
             } else if (ch == ']' || ch == '}' || ch == ')') {
+                // If a closer is encountered, decrease brackets by 1 and then look for additional functionality based on the type of closer
                 brackets--;
+
+                /*
+                 *The idea of the if-else branches below are to determine whether the currently selected closer character is of the same type as the most recently encountered opener character that is stored at the end of the openingBrackets ArrayList.
+                 *
+                 * First, we check to see if the openingBrackets ArrayList is NOT empty, because we do not want to try and access an element at index -1 (if we didn't check and this was encountered an error would be thrown.
+                 *
+                 * Once openingBrackets has been validated as NOT empty, we then compare the last element of the list to the corresponding opener of the closing character.
+                 * * If they do not match, then we can assume that the String is unbalanced because the current closer should always be of matching type to last opener
+                 * * If the types do match, then that segment of the String is balanced. We remove the last element of the openingCharacters ArrayList so we can compare next encountered closer with the next opener now stored at the new end of the list.
+                 */
 
                 if (!openingBrackets.isEmpty() && ch == ']') {
                     if (openingBrackets.get(openingBrackets.size() - 1) != '[') {
+                        // If '[' & ']' do not correspond, String is unbalanced
                         return false;
                     } else {
                         openingBrackets.remove(openingBrackets.size() - 1);
                     }
                 } else if (!openingBrackets.isEmpty() && ch == '}') {
                     if (openingBrackets.get(openingBrackets.size() - 1) != '{') {
+                        // If '{' & '}' do not correspond, String is unbalanced
                         return false;
                     } else {
                         openingBrackets.remove(openingBrackets.size() - 1);
                     }
                 } else if (!openingBrackets.isEmpty() && ch == ')') {
+                    // If '(' & ')' do not correspond, String is unbalanced
                     if (openingBrackets.get(openingBrackets.size() - 1) != '(') {
                         return false;
                     } else {
                         openingBrackets.remove(openingBrackets.size() - 1);
                     }
                 } else if (openingBrackets.isEmpty() && (ch == ']' || ch == '}' || ch == ')')) {
+                    // If there are no elements in the openingBrackets list and there is still an instance of a closer, then the String is unbalanced
                     return false;
                 }
-//                closingBrackets.add(ch);
             }
         }
-/*
-        int openingBracketsIndex = openingBrackets.size() - 1;
-        int closingBracketsIndex = 0;
 
-        while(openingBracketsIndex >= 0 && closingBracketsIndex < closingBrackets.size()) {
-            if(openingBrackets.size() != closingBrackets.size()) {
-                return false;
+        /*
+         * If brackets does not equal zero, then there is at least one extra opener or closer and the String is unbalanced
+         * If brackets equals zero, then the String is balanced
+         */
 
-            } else if (openingBrackets.get(openingBracketsIndex) == '[' && closingBrackets.get(closingBracketsIndex) == ']') {
-                System.out.println("Braces match.");
-
-                openingBracketsIndex--;
-                closingBracketsIndex++;
-
-            } else if (openingBrackets.get(openingBracketsIndex) == '{' && closingBrackets.get(closingBracketsIndex) == '}') {
-                System.out.println("Braces match.");
-
-                openingBracketsIndex--;
-                closingBracketsIndex++;
-
-            } else if (openingBrackets.get(openingBracketsIndex) == '(' && closingBrackets.get(closingBracketsIndex) == ')') {
-                System.out.println("Braces match.");
-
-                openingBracketsIndex--;
-                closingBracketsIndex++;
-
-            } else {
-                System.out.println("Braces mismatch");
-                return false;
-            }
-        }*/
         return brackets == 0;
     }
 
-//    public static void main(String[] args) {
-//        System.out.println(BalancedBrackets.hasBalancedBrackets("[[]"));
-//    }
 }
